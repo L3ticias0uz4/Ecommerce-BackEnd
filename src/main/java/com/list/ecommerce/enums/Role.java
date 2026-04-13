e6c08630400dc89c8ea3b72b9061bd3b10283550
L3ticias0uz4 <leticiasouza56000@gmail.com>
2026-04-13 11:04:14 -0300

    updating ecommerce code

--- src/main/java/com/list/ecommerce/enums/Role.java
+++ src/main/java/com/list/ecommerce/enums/Role.java
@@ -1,4 +1,6 @@
 package com.list.ecommerce.enums;
 
 public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER
 }
--- src/main/java/com/list/ecommerce/repository/UsuarioRepository.java
+++ src/main/java/com/list/ecommerce/repository/UsuarioRepository.java
@@ -4,11 +4,15 @@ import com.list.ecommerce.entity.Usuario;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Optional<Usuario> findByEmail(@NotBlank(message = "O campo não pode estar vazio") @Email(message = "Digite um email valido") String email);
 
     Integer id(long id);
+
+
 }
